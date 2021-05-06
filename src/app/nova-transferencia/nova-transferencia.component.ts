95d6165b7064bc1d6052a2bb610d3f964831af40
Marco Aurélio Monteiro Lima <marco.monteirolima@gmail.com>
2021-05-05 22:44:14 -0300

    commit

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, Output } from "@angular/core";
+import * as EventEmitter from "node:events";
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -8,13 +9,20 @@ import { Component } from "@angular/core";
 
 export class NovaTransferenciaComponent {
 
-  valor: number = 222;
-  destino: number = 222;
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
 
   transferir(){
     console.log("Solicitado nova transferência");
-    console.log("Valor: ", this.valor);
-    console.log("Destino: ", this.destino);
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 
 }
